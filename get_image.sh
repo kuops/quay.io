ebf6a59f97887e7b9170e8d4d698c5921d4398b0
kuops <opshsy@gmail.com>
2018-07-16 10:18:39 -0400

    fix get_image.sh

--- get_image.sh
+++ get_image.sh
@@ -2,7 +2,7 @@
 
 QUAY_NAMESPACE=(calico coreos prometheus)
 DOCKERHUB_NAMESPACE=kuopsquay
-QUAY_REPO_RRE="quay.io"
+QUAY_REPO_PRE="quay.io"
 REPO_API_URL="https://quay.io/api/v1/repository?public=true&namespace="
 
 today(){
